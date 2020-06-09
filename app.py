aec29c422581d23614a2abc0ef6d0f95ca3c2e33
Ivan Georgiev <ivan.georgiev@nl.abnamro.com>
2020-06-09 11:51:09 +0200

    Get SQL Version

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
-from flask import Flask
+import os
+import struct
+import pyodbc
+from azure.identity import DefaultAzureCredential
+
+from flask import Flask, jsonify
+
 app = Flask(__name__)
+
+
+
+def connect_db(server=None, database=None, driver=None):
+    DB_SERVER = 'tcp:' + (server or os.environ['DB_SERVER']) + '.database.windows.net'
+    DB_NAME = database or os.environ['DB_NAME']
+    DB_DRIVER = driver or '{ODBC Driver 17 for SQL Server}'
+    DB_RESOURCE_URI = 'https://database.windows.net/'
+
+    az_credential = DefaultAzureCredential()
+    access_token = az_credential.get_token(DB_RESOURCE_URI)
+    
+    token = bytes(access_token.token, 'utf-8')
+    exptoken = b"";
+    for i in token:
+        exptoken += bytes({i});
+        exptoken += bytes(1);
+    tokenstruct = struct.pack("=i", len(exptoken)) + exptoken;
+    
+    connection_string = 'driver='+DB_DRIVER+';server='+DB_SERVER+';database='+DB_NAME
+    conn = pyodbc.connect(connection_string, attrs_before = { 1256:bytearray(tokenstruct) });
+    
+    return conn
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    db = connect_db()
+    result = {}
+    with db.execute("SELECT @@version") as resultset:
+        # result['columns'] = row.cursor_description
+        result['rows']  = [list(row) for row in resultset]
+    return jsonify(result)
